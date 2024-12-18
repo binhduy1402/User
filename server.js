533340cf25528929a52f200d3ae6237543ed9396
Bình Duy <166035036+binhduy1402@users.noreply.github.com>
2024-12-18 11:51:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const cors=require("cors");
-const dotenv=require("dotenv");
-const connectDB =require('./connection');
-const app=express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require('./connection');
+const app = express();
 const cookieParser = require("cookie-parser");
-dotenv.config()
+dotenv.config();
+
 var multer = require('multer');
 
 const PORT = process.env.PORT || 8070;
@@ -14,23 +15,24 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors(
-    { 
-       origin:["http://localhost:3000"],
-       credentials: true,
-    }
-));
+app.use(cors({
+    origin: [
+        "http://localhost:3000",  // Local development URL
+        "https://fe-rfyq.onrender.com"  // Allow the frontend from Render
+    ],
+    credentials: true,  // Allow cookies to be sent across origins
+}));
 
 app.use(bodyParser.json());
 
-//User Management
-const UserManagement=require('./api/UserManagement/UserManagement.api');
-app.use('/user-management',UserManagement);
+// User Management
+const UserManagement = require('./api/UserManagement/UserManagement.api');
+app.use('/user-management', UserManagement);
 
-//Authentication
-const Authentication=require('./api/Authentication/Auth.api');
-app.use('/auth',Authentication);
+// Authentication
+const Authentication = require('./api/Authentication/Auth.api');
+app.use('/auth', Authentication);
 
-app.listen(PORT,() =>{
+app.listen(PORT, () => {
     console.log('User management service is running');
 });
